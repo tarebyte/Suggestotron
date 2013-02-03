b3efa07d983fa927dbb19b1ac0c8df32b7df8e4f
Dave Strock <dave.strock@gmail.com>
2013-02-03 15:53:16 -0600

    Redirects from root to topics#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Suggestotron::Application.routes.draw do
   resources :votes
-
-
   resources :topics
 
+  root :to => 'topics#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
